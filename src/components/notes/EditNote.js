6ff69dc3526733b9e5d5ddc64d015c82bc505283
Mike Maher <mike.maher@exchequer.ie>
2018-09-30 10:03:49 +0100

    editor debugging

--- src/components/notes/EditNote.js
+++ src/components/notes/EditNote.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getNote } from '../../actions/noteActions';
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  getDefaultKeyBinding,
+  ContentState,
+  convertToRaw,
+  convertFromRaw
+} from 'draft-js';
+import BlockStyleControls from '../editor/BlockStyleControls';
+import InlineStyleControls from '../editor/InlineStyleControls';
 
 class EditNote extends Component {
   state = {
     id: '',
     title: '',
-    note: {}
+    note: {},
+    editorState: EditorState.createEmpty()
   };
 
   componentWillReceiveProps(nextProps, nextState) {
@@ -23,14 +36,96 @@ class EditNote extends Component {
     const { id } = this.props.match.params;
     this.props.getNote(id);
   }
+
+  focus = () => this.refs.editor.focus();
+  onChange = editorState => this.setState({ editorState });
+  onInputChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+  mapKeyToEditorCommand(e) {
+    if (e.keyCode === 9 /* TAB */) {
+      const newEditorState = RichUtils.onTab(
+        e,
+        this.state.editorState,
+        4 /* maxDepth */
+      );
+      if (newEditorState !== this.state.editorState) {
+        this.onChange(newEditorState);
+      }
+      return;
+    }
+    return getDefaultKeyBinding(e);
+  }
+  toggleBlockType(blockType) {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
+  }
+  toggleInlineStyle(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle)
+    );
+  }
   render() {
-    const { id, title, note } = this.state;
+    const { id, title, note, editorState } = this.state;
+    // let className = 'RichEditor-editor';
+    // let contentState = editorState.createWithContent(
+    //   ContentState.createFromBlockArray(convertToRaw(note.blocks))
+    // );
+    // if (!contentState.hasText()) {
+    //   if (
+    //     contentState
+    //       .getBlockMap()
+    //       .first()
+    //       .getType() !== 'unstyled'
+    //   ) {
+    //     className += ' RichEditor-hidePlaceholder';
+    //   }
+    // }
     return (
-      <div>
-        <span>id = {id}</span>
-        <span>title = {title}</span>
-        <div>note = {note.toString()}</div>
-      </div>
+      <React.Fragment>
+        <form>
+          <input
+            id="note-title"
+            className="form-control"
+            name="title"
+            type="text"
+            placeholder="Enter Title"
+            value={title}
+            onChange={this.onInputChange}
+          />
+          {/* <div className="RichEditor-root">
+            <BlockStyleControls
+              editorState={editorState}
+              onToggle={this.toggleBlockType}
+            />
+            <InlineStyleControls
+              editorState={editorState}
+              onToggle={this.toggleInlineStyle}
+            />
+            <div className={className} onClick={this.focus}>
+              <Editor
+                blockStyleFn={getBlockStyle}
+                customStyleMap={styleMap}
+                editorState={editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                keyBindingFn={this.mapKeyToEditorCommand}
+                onChange={this.onChange}
+                placeholder="Make a Note..."
+                ref="editor"
+                spellCheck={true}
+              />
+            </div>
+          </div> */}
+          <button type="submit" className="btn btn-primary">
+            <i className="fa fa-save" />
+          </button>
+        </form>
+      </React.Fragment>
     );
   }
 }
@@ -44,9 +139,29 @@ const mapStateToProps = state => ({
   note: state.note.note
 });
 
-export default connect(
-  mapStateToProps,
-  {
-    getNote
+const styleMap = {
+  CODE: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2
   }
-)(EditNote);
+};
+
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case 'blockquote':
+      return 'RichEditor-blockquote';
+    default:
+      return null;
+  }
+}
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {
+      getNote
+    }
+  )(EditNote)
+);
